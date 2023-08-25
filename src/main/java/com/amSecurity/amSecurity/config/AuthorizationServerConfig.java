14c8122de5082b3bd536fcb43c2962122ad70f60
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-08-25 12:46:25 +0530

    configured -> ResourceServerConfig

--- src/main/java/com/amSecurity/amSecurity/config/AuthorizationServerConfig.java
+++ src/main/java/com/amSecurity/amSecurity/config/AuthorizationServerConfig.java
@@ -44,7 +44,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     }
 
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception{
-        endpoints.tokenStore(tokenStore).authenticationManager(authenticationManager);
+        endpoints
+                .tokenStore(tokenStore)
+                .authenticationManager(authenticationManager);
     }
 
 
