fc401cf703dbc85baffebb34900a662e38509414
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-08-25 10:59:33 +0530

    uploading project

---
+++ src/main/java/com/amSecurity/amSecurity/AmSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.amSecurity.amSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amSecurity/amSecurity/AmSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amSecurity.amSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
