671d0a3c7525afeb6cca36ba2a10e6a8b78cb190
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-09-11 13:24:19 +0530

    update project -> JWT Authentication

--- src/main/java/com/amSecurity/amSecurity/config/SecurityConfig.java
+++ src/main/java/com/amSecurity/amSecurity/config/SecurityConfig.java
@@ -1,21 +1,28 @@
 package com.amSecurity.amSecurity.config;
 
 
+import com.amSecurity.amSecurity.filters.JwtRequestFilter;
 import com.amSecurity.amSecurity.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -24,11 +31,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasAnyRole("USER","ADMIN")
-                .antMatchers("/").permitAll()
-                .and().formLogin();
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
--- src/main/java/com/amSecurity/amSecurity/controller/MainController.java
+++ src/main/java/com/amSecurity/amSecurity/controller/MainController.java
@@ -1,24 +1,53 @@
 package com.amSecurity.amSecurity.controller;
 
+import com.amSecurity.amSecurity.model.AuthenticationRequest;
+import com.amSecurity.amSecurity.model.AuthenticationResponse;
+import com.amSecurity.amSecurity.service.MyUserDetailsService;
+import com.amSecurity.amSecurity.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 public class MainController {
 
-    @GetMapping("/")
-    public String welcomeMethod(){
-        return "<h1> Welcome to Demo Application!!</h1>";
-    }
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-    @GetMapping("/user")
-    public String welcomeUser(){
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
+    @GetMapping("/loggedUser")
+    public String welcomeLoggedUser(){
         return "<h1> Welcome System User to Demo Application!!</h1>";
     }
 
-    @GetMapping("/admin")
-    public String welcomeAdmin(){
-        return "<h1> Welcome System Admin to Demo Application!!</h1>";
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticateToken(@RequestBody AuthenticationRequest authRequest)throws Exception{
+
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUserName(),authRequest.getPassword())
+            );
+        }catch (BadCredentialsException exception){
+            throw new Exception("Incorrect UserName or Password -> ",exception);
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authRequest.getUserName());
+
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+
     }
 
 
